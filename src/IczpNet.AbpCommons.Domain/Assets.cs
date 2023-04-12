c748357e09f94849a4d699f39fb39171608c0d28
zhongpei <1000@intry.cn>
2023-04-12 14:04:31 +0800

    Assets

--- src/IczpNet.AbpCommons.Domain/Assets.cs
+++ src/IczpNet.AbpCommons.Domain/Assets.cs
@@ -1,4 +1,5 @@
 ﻿using JetBrains.Annotations;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -12,48 +13,30 @@ namespace IczpNet.AbpCommons
         [ContractAnnotation("value:true => halt")]
         public static void If(
             bool value,
-            [InvokerParameterName][NotNull] string message)
-        {
-            if (value)
-            {
-                throw new UserFriendlyException(message);
-            }
-        }
-
-        [ContractAnnotation("value:true => halt")]
-        public static void If(
-           bool value,
-           [InvokerParameterName][NotNull] string message,
-           [InvokerParameterName][NotNull] string parameterName)
-        {
-            if (value)
-            {
-                throw new UserFriendlyException(message, parameterName);
-            }
-        }
-
-        [ContractAnnotation("value:null => halt")]
-        public static T NotNull<T>(
-            T value,
-            [InvokerParameterName][NotNull] string message)
-        {
-            if (value == null)
-            {
-                throw new UserFriendlyException(message);
-            }
-
-            return value;
-        }
-
-        [ContractAnnotation("value:null => halt")]
-        public static T NotNull<T>(
-            T value,
             [InvokerParameterName][NotNull] string message,
-            [InvokerParameterName][NotNull] string parameterName)
+            string code = null,
+            string details = null,
+            Exception innerException = null,
+            LogLevel logLevel = LogLevel.Warning)
+        {
+            if (value)
+            {
+                throw new UserFriendlyException(message, code, details, innerException, logLevel);
+            }
+        }
+
+
+        [ContractAnnotation("value:null => halt")]
+        public static T NotNull<T>(
+            T value,
+            [InvokerParameterName][NotNull] string message, string code = null,
+            string details = null,
+            Exception innerException = null,
+            LogLevel logLevel = LogLevel.Warning)
         {
             if (value == null)
             {
-                throw new UserFriendlyException(message, parameterName);
+                throw new UserFriendlyException(message, code, details, innerException, logLevel);
             }
 
             return value;
@@ -64,21 +47,24 @@ namespace IczpNet.AbpCommons
             string value,
             [InvokerParameterName][NotNull] string parameterName,
             int maxLength = int.MaxValue,
-            int minLength = 0)
+            int minLength = 0,
+            string details = null,
+            Exception innerException = null,
+            LogLevel logLevel = LogLevel.Warning)
         {
             if (value == null)
             {
-                throw new UserFriendlyException($"{parameterName} 不能为空!", parameterName);
+                throw new UserFriendlyException($"{parameterName} 不能为空!", parameterName, details, innerException, logLevel);
             }
 
             if (value.Length > maxLength)
             {
-                throw new UserFriendlyException($"{parameterName} 长度必须小于或等于 {maxLength}!", parameterName);
+                throw new UserFriendlyException($"{parameterName} 长度必须小于或等于 {maxLength}!", parameterName, details, innerException, logLevel);
             }
 
             if (minLength > 0 && value.Length < minLength)
             {
-                throw new UserFriendlyException($"{parameterName} 长度必须大于或等于 {minLength}!", parameterName);
+                throw new UserFriendlyException($"{parameterName} 长度必须大于或等于 {minLength}!", parameterName, details, innerException, logLevel);
             }
 
             return value;
@@ -89,46 +75,44 @@ namespace IczpNet.AbpCommons
             string value,
             [InvokerParameterName][NotNull] string parameterName,
             int maxLength = int.MaxValue,
-            int minLength = 0)
+            int minLength = 0, string details = null,
+            Exception innerException = null,
+            LogLevel logLevel = LogLevel.Warning)
         {
             if (value.IsNullOrWhiteSpace())
             {
-                throw new UserFriendlyException($"{parameterName} 字符不能为空!", parameterName);
+                throw new UserFriendlyException($"{parameterName} 字符不能为空!", parameterName, details, innerException, logLevel);
             }
 
             if (value.Length > maxLength)
             {
-                throw new UserFriendlyException($"{parameterName} 字符长度必须小于或等于 {maxLength}!", parameterName);
+                throw new UserFriendlyException($"{parameterName} 字符长度必须小于或等于 {maxLength}!", parameterName, details, innerException, logLevel);
             }
 
             if (minLength > 0 && value.Length < minLength)
             {
-                throw new UserFriendlyException($"{parameterName} 字符长度必须大于或等于 {minLength}!", parameterName);
+                throw new UserFriendlyException($"{parameterName} 字符长度必须大于或等于 {minLength}!", parameterName, details, innerException, logLevel);
             }
 
             return value;
         }
 
         [ContractAnnotation("value:null => halt")]
-        public static string NotNullOrEmpty(
-            string value,
-            [InvokerParameterName][NotNull] string parameterName,
-            int maxLength = int.MaxValue,
-            int minLength = 0)
+        public static string NotNullOrEmpty(string value, [InvokerParameterName][NotNull] string parameterName, int maxLength = int.MaxValue, int minLength = 0, string details = null, Exception innerException = null, LogLevel logLevel = LogLevel.Warning)
         {
             if (value.IsNullOrEmpty())
             {
-                throw new UserFriendlyException($"{parameterName} 字符不能为空!", parameterName);
+                throw new UserFriendlyException($"{parameterName} 字符不能为空!", parameterName, details, innerException, logLevel);
             }
 
             if (value.Length > maxLength)
             {
-                throw new UserFriendlyException($"{parameterName} 字符长度必须小于或等于 {maxLength}!", parameterName);
+                throw new UserFriendlyException($"{parameterName} 字符长度必须小于或等于 {maxLength}!", parameterName, details, innerException, logLevel);
             }
 
             if (minLength > 0 && value.Length < minLength)
             {
-                throw new UserFriendlyException($"{parameterName} 字符长度必须大于或等于 {minLength}!", parameterName);
+                throw new UserFriendlyException($"{parameterName} 字符长度必须大于或等于 {minLength}!", parameterName, details, innerException, logLevel);
             }
 
             return value;
@@ -164,24 +148,26 @@ namespace IczpNet.AbpCommons
             [CanBeNull] string value,
             [InvokerParameterName][NotNull] string parameterName,
             int maxLength,
-            int minLength = 0)
+            int minLength = 0, string details = null,
+        Exception innerException = null,
+        LogLevel logLevel = LogLevel.Warning)
         {
             if (minLength > 0)
             {
                 if (string.IsNullOrEmpty(value))
                 {
-                    throw new UserFriendlyException(parameterName + " 为能为null或为空!", parameterName);
+                    throw new UserFriendlyException(parameterName + " 为能为null或为空!", parameterName, details, innerException, logLevel);
                 }
 
                 if (value.Length < minLength)
                 {
-                    throw new UserFriendlyException($"{parameterName} 长度必须大于或等于 {minLength}!", parameterName);
+                    throw new UserFriendlyException($"{parameterName} 长度必须大于或等于 {minLength}!", parameterName, details, innerException, logLevel);
                 }
             }
 
             if (value != null && value.Length > maxLength)
             {
-                throw new UserFriendlyException($"{parameterName} 长度必须小于或等于 {maxLength}!", parameterName);
+                throw new UserFriendlyException($"{parameterName} 长度必须小于或等于 {maxLength}!", parameterName, details, innerException, logLevel);
             }
 
             return value;
